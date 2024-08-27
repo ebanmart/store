461b133f4d17826efd0a31c3d92e3d0bceb0c60f
FazlyRabbi <rabbisam74@gmail.com>
2024-08-27 17:07:24 +0600

    add facebook pixle code

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
-//internal import
-
+// Internal imports
 import Navbar from "@layout/navbar/Navbar";
 import Footer from "@layout/footer/Footer";
 import NavBarTop from "./navbar/NavBarTop";
@@ -16,9 +15,38 @@ const Layout = ({ title, description, children }) => {
       <ToastContainer />
       <div className="font-sans">
         <Head>
-          <title>{title ? `Ebanmart | ${title}` : "Ebanmart -Store"}</title>
+          <title>{title ? `Ebanmart | ${title}` : "Ebanmart - Store"}</title>
           {description && <meta name="description" content={description} />}
-          <link ref="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.png" />
+          
+          {/* Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '854362180004219');
+              fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=854362180004219&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+          {/* End Meta Pixel Code */}
+
         </Head>
         {/* <NavBarTop /> */}
         <Navbar />
@@ -32,7 +60,7 @@ const Layout = ({ title, description, children }) => {
             <FeatureCard />
           </div> */}
 
-          <hr className="hr-line"></hr>
+          <hr className="hr-line" />
           <div className="border-t border-gray-100 w-full">
             <Footer />
           </div>
